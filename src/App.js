528df842d482ce6d59549612deeaad0a41532d3d
AstrydCasimir <astryd.casimirgressier@gmail.com>
2023-11-21 15:45:11 +0100

    Update TodoForm.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { TodoWrapper } from './components/TodoWrapper';
+import { TodoForm } from './components/TodoForm';
 
 function App() {
   return (
     <div className="App">
       <TodoWrapper />
+      <TodoForm />
     </div>
   );
 }
